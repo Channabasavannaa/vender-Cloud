a9b451f145327c6af0cbf172793b66a39ccb08d6
Channabasavannaa <channabasavannamb@gmail.com>
2025-11-11 00:27:10 +0530

    adding records

--- database/src/dydb.ts
+++ database/src/dydb.ts
@@ -1,8 +1,11 @@
 import { 
   DynamoDBClient, 
   CreateTableCommand, 
-  CreateTableCommandInput,DescribeTableCommand 
+  CreateTableCommandInput,DescribeTableCommand, DeleteTableCommand, PutItemCommand
 } from "@aws-sdk/client-dynamodb";
+
+import { Vender } from "./types/twitter";
+import { marshall } from "@aws-sdk/util-dynamodb";
 
 // Create a DynamoDB client
 const client = new DynamoDBClient({
@@ -30,16 +33,47 @@ export const dynamodbCreateTable = async (params: CreateTableCommandInput) => {
 export const dynamodbDescribeTable = async (tableName: string) => {
     try{
         const table = await client.send(new DescribeTableCommand({ TableName: tableName }));
-        console.log("✅ Table description:", table.Table);
+        console.log(" Table description:", table.Table);
         return table;
     }
     catch(err) {
     if (err instanceof Error) {
-      console.error("❌ Error:", err.message);
+      console.error("Error:", err.message);
       throw err;
     }
-    throw new Error("Unknown error occurred while creating table");
+    throw new Error("Unknown error occurred while describing table");
   }
 
     
 }
+
+//deleteing a table function
+
+export const dynamodbDeleteTable = async (tableName: string) => {
+    try{
+         const result = await client.send(new DeleteTableCommand({ TableName: tableName }));
+        console.log(" Table deleted:", result);
+        return result;
+
+    }catch(err) {
+    if (err instanceof Error) {
+      console.error(" Error:", err.message);
+      throw err;
+    }
+}};
+
+// creating a record function
+export const dynamodbCreateRecord = async (tableName: string, vender: Vender) => {
+    try{
+        const record = await client.send(new PutItemCommand({ TableName: tableName, Item: marshall(vender) }));
+        console.log("record inserted ", record);
+        return record;
+
+    }catch(err) {
+    if (err instanceof Error) {
+      console.error(" Error:", err.message);
+      throw err;
+    }
+}
+};
+
--- database/src/index.ts
+++ database/src/index.ts
@@ -1,15 +1,20 @@
-
-
-//const { dynamodbCreateTable } = require("./dydb");
+import { Vender } from "./types/twitter";
+import venders from "./data/venders";
+import { dynamodbDeleteTable } from "./dydb";
+import { dynamodbCreateRecord } from "./dydb";
+const { dynamodbCreateTable } = require("./dydb");
 
 const { dynamodbDescribeTable } = require("./dydb");
 
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
 
 const init = async () => {  
+    const venderTablename = "Vendors";
 
-await dynamodbDescribeTable("Vendors");
-const vendorstableParams = {
-  TableName: "Vendors",
+
+  const vendorstableParams = {
+  TableName: venderTablename,
   KeySchema: [{ AttributeName: "twitterId", KeyType: "HASH" }],
   AttributeDefinitions: [{ AttributeName: "twitterId", AttributeType: "S" }],
   ProvisionedThroughput: {
@@ -18,8 +23,36 @@ const vendorstableParams = {
   },
 };
 
-//dynamodbCreateTable(vendorstableParams);
+
+try {
+const venderTable = await dynamodbDescribeTable(venderTablename);
+if (!(venderTable instanceof Error)) {
+    // Define table parameters
+    await dynamodbDeleteTable(venderTablename);
+    await delay(10000);
+
+    await dynamodbCreateTable(vendorstableParams);
+    await delay(10000);
 
 }
+}catch(err){
+  await dynamodbCreateTable(vendorstableParams);
+  await delay(10000);
+}
+
+
+
+for(const i in venders){
+
+  const vender = venders[i];
+ if (!vender) continue; 
+  try {
+    await dynamodbCreateRecord(venderTablename, vender);
+  } catch (err) {
+    console.log("Failed to create record for vender:", vender.name, err);
+  }
+
+}
+};
 
 init();
